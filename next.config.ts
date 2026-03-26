adf6391dabf5fad281091766db2b9ffe9a5ee4d2
TskJafri <taskeen.s.jafri@gmail.com>
2026-03-25 19:22:16 -0700

    V1.0.1 - Deploy on github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.GITHUB_ACTIONS === "true";
+const repo = "OxEco"; // your repo name
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: isGithubPages ? `/${repo}` : "",
+  assetPrefix: isGithubPages ? `/${repo}/` : "",
 };
 
 export default nextConfig;
